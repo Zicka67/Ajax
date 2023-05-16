906d39e6772f0a89d08854842e6382e69267e053
Zicka67 <art.book@hotmail.fr>
2023-05-16 14:20:50 +0200

    Modification du JS + commentaires

--- assets/app.js
+++ assets/app.js
@@ -9,24 +9,28 @@ const videosList = document.querySelector('#videos_list');
 formVideo.addEventListener('submit', function (e) {
     e.preventDefault();
 
+   
+    // Effectue une requête POST vers l'URL spécifiée dans action du formulaire
     fetch(this.action, {
-        body: new FormData(e.target),
+        body: new FormData(e.target),  // Récupère les données du formulaire
         method: 'POST'
     })
-        .then(response => response.json())
+        .then(response => response.json())  // Convertit la réponse en JSON
         .then(json => {
-            handleResponse(json);
+            // console.log(json); 
+            handleResponse(json);  // Appelle "handleResponse" en lui passant la réponse JSON
+        })
+        .catch(e => {
+            console.error('problème avec: ' + e.message);
         });
 });
 
 const handleResponse = function (response) {
-    // removeErrors();
+    // console.log('Handling response', response); 
+
     switch(response.code) {
         case 'VIDEO_ADDED_SUCCESSFULLY':
-            videosList.insertAdjacentHTML('beforeend', response.html);
+            videosList.insertAdjacentHTML('beforeend', response.html);  // Insère le HTML de la réponse à la fin de la liste de vidéos
             break;
-        // case 'VIDEO_INVALID_FORM':
-        //     handleErrors(response.errors);
-        //     break;
     }
 }
