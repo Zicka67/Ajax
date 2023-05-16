f0b7ec3cf4babf8fafad385b28f216d167c8771f
Zicka67 <art.book@hotmail.fr>
2023-05-16 10:10:57 +0200

    Ajout dune mignature et ajout des directory dans les parameters du services.yaml et début de JS

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,20 @@
 import './styles/app.css';
 
 import './styles/bootstrap.min.css'; 
+
+const formVideo = document.querySelector('#form_video');
+const videosList = document.querySelector('#videos_list');
+
+
+formVideo.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    fetch(this.action, {
+        body: new FormData(e.target),
+        method: 'POST'
+    })
+        .then(response => response.json())
+        .then(json => {
+            handleResponse(json);
+        });
+});
